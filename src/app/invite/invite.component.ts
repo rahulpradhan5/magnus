85173f573a7590f3b75c8d24716099cbd25802ed
rahulpradhan5 <prahulkumar591@gmail.com>
2023-04-22 12:46:34 +0530

    hover tree section changed

--- src/app/invite/invite.component.ts
+++ src/app/invite/invite.component.ts
@@ -10,19 +10,34 @@ import { Router } from '@angular/router';
   styleUrls: ['./invite.component.css']
 })
 export class InviteComponent implements OnInit {
-  reffer?:string='';
-  uid?:any='';
-  url?:string;
-  constructor(fStore:AngularFirestore , public auth:AngularFireAuth,private storage:AngularFireStorage ,public route:Router) {
-    this.auth.user.subscribe(user=>{
+  reffer?: any = '';
+  uid?: any = '';
+  url?: any;
+  status: boolean = false;
+  constructor(fStore: AngularFirestore, public auth: AngularFireAuth, private storage: AngularFireStorage, public route: Router) {
+    this.auth.user.subscribe(user => {
       this.uid = user?.displayName;
-     });
-    this.url =   window.location.origin
-   }
-   onclick(data:string) {
-    this.reffer = this.uid+"/"+data;
-   }
+    });
+    this.url = window.location.origin
+    this.reffer = this.url;
+  }
+  onclick(data: string) {
+    this.reffer =  this.url+"/"+this.uid + "/" + data;
+  }
   ngOnInit(): void {
+  }
+  copyTextEnable(){
+    this.status = true;
+  }
+  copyText() {
+    // Select the input element and copy the generated link to the clipboard
+    const inputElement = document.createElement('input');
+    inputElement.value = this.reffer;
+    document.body.appendChild(inputElement);
+    inputElement.select();
+    document.execCommand('copy');
+    alert('Link copied!');
+    document.body.removeChild(inputElement);
   }
 
 }
